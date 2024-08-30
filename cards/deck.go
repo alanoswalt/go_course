deffdf31de7d7dfe2c0d6ece2203a8f09b17dbbc
Alan <alanoswalt01@gmail.com>
2024-08-29 18:43:21 -0600

    New function

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func deck() {
-	fmt.Println("Hello World")
+//Create a new type of eck
+type deck []string
+
+func newDeck() deck {
+	cards2 := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Dimonds", "Clubs"}
+
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, value := range cardValues {
+
+		for _, suit := range cardSuits {
+			cards2 = append(cards2, value+" of "+suit)
+		}
+	}
+
+	return cards2
+}
+
+//
+func (d deck) print_cards() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
+
+//Two inputs and two outputs
+//Both outputs are type deck
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- cards/main.go
+++ cards/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	//Deck is a new type created in deck.go
+	//deck == []string
+	cards := newDeck()
+	//cards2 := []string{newCard(), "3 OF hearts"}
+
+	//cards.print_cards()
+
+	hand, remainingDeck := deal(cards, 5)
+	hand.print_cards()
+	remainingDeck.print_cards()
+
 }
