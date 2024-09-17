90b76c1200fa3e640fcfb98d8e23a83a66d5ab66
Alan <alanoswalt01@gmail.com>
2024-09-17 17:19:59 -0600

    New excersice

--- cards/deck.go
+++ cards/deck.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
 
-//Create a new type of eck
+// Create a new type of eck
 type deck []string
 
 func newDeck() deck {
@@ -22,15 +27,52 @@ func newDeck() deck {
 	return cards2
 }
 
-//
 func (d deck) print_cards() {
 	for _, card := range d {
 		fmt.Println(card)
 	}
 }
 
-//Two inputs and two outputs
-//Both outputs are type deck
+// Two inputs and two outputs
+// Both outputs are type deck
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) deckToString() string {
+
+	//La funcion join de la libreria string toma un array de strings y los junta con el separado de coma
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.deckToString()), 066)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+
+	if err != nil {
+		// Option 1 - log error and call new deck
+		// Option 2 - Log error and quit the program
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	//Bs is a []byte, chance to string with "string(bs)"
+	//Separate that long string to a []string with split
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+
+}
+
+func (d deck) shuffledeck() {
+	for index, _ := range d {
+
+		random_number := rand.Intn(len(d) - 1)
+
+		d[index], d[random_number] = d[random_number], d[index]
+	}
+
 }
--- cards/main.go
+++ cards/main.go
@@ -3,17 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	//fmt.Println("Hello World")
 
 	//Deck is a new type created in deck.go
 	//deck == []string
-	cards := newDeck()
+	//cards := newDeck()
 	//cards2 := []string{newCard(), "3 OF hearts"}
 
 	//cards.print_cards()
 
-	hand, remainingDeck := deal(cards, 5)
-	hand.print_cards()
-	remainingDeck.print_cards()
+	//hand, remainingDeck := deal(cards, 5)
+	//hand.print_cards()
+	//remainingDeck.print_cards()
+
+	cards := newDeck()
+
+	fmt.Println(cards.deckToString())
+
+	cards.saveToFile("mycards")
+
+	cards.shuffledeck()
+
+	cards.print_cards()
+	//cards2 := newDeckFromFile("mycards")
+
+	//cards2.print_cards()
 
 }
